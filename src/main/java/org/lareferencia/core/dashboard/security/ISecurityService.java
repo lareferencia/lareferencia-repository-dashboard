87ff066aa55d93a032fdc8d5cca40f67353e1e64
lmatas <lmatas@gmail.com>
2021-01-28 10:28:16 +0100

    User managment service - to be improved

--- src/main/java/org/lareferencia/core/dashboard/security/ISecurityService.java
+++ src/main/java/org/lareferencia/core/dashboard/security/ISecurityService.java
@@ -1,14 +1,31 @@
 package org.lareferencia.core.dashboard.security;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 public interface ISecurityService {
 	
 	List<String> getRequestGroups(HttpServletRequest request);
-	
 	Boolean isAdminRequest(HttpServletRequest request);
- 
-
+	
+	/**********************/ 
+	
+	Boolean createUser(String userId, Map<String,String> infoMap);
+	Boolean deleteUser(String userId, Map<String,String> infoMap);
+	Boolean updateUser(String userId, Map<String,String> infoMap);
+	
+	Boolean changePassword(String userId, String passwd);
+	
+	Map<String,String> getUserInfo(String userId);
+	
+	Boolean createGroup(String groupId, Map<String,String> infoMap);
+	Boolean deleteGroup(String groupId);
+	Boolean updateGroup(String groupId, Map<String,String> infoMap);
+	
+	Map<String,String> getGroupInfo(String groupId);
+	
+	Boolean addUserToGroup(String userId, String groupId);
+	
 }
--- src/main/java/org/lareferencia/core/dashboard/security/KeycloakSecurityService.java
+++ src/main/java/org/lareferencia/core/dashboard/security/KeycloakSecurityService.java
@@ -1,6 +1,7 @@
 package org.lareferencia.core.dashboard.security;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -34,6 +35,66 @@ public class KeycloakSecurityService implements ISecurityService {
 	
 	private KeycloakSecurityContext getKeycloakSecurityContext(HttpServletRequest request) {
 		return (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
+	}
+
+	@Override
+	public Boolean createUser(String userId, Map<String, String> infoMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean deleteUser(String userId, Map<String, String> infoMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean updateUser(String userId, Map<String, String> infoMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean changePassword(String userId, String passwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, String> getUserInfo(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean createGroup(String groupId, Map<String, String> infoMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean deleteGroup(String groupId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean updateGroup(String groupId, Map<String, String> infoMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, String> getGroupInfo(String groupId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean addUserToGroup(String userId, String groupId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
