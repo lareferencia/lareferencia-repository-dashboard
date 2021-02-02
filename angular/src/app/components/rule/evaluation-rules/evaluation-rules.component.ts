8a1d8f4c9e011dcaa5cb102b80277e3f1d2a1052
André Andreotti <dias.andreotti@gmail.com>
2021-02-02 20:20:10 -0300

    Add filter and legend in record details
    Add filter and legend in record details

--- angular/src/app/components/rule/evaluation-rules/evaluation-rules.component.ts
+++ angular/src/app/components/rule/evaluation-rules/evaluation-rules.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Record } from '../../../shared/models/record.model';
 
@@ -9,13 +10,64 @@ import { Record } from '../../../shared/models/record.model';
 })
 export class EvaluationRulesComponent implements OnInit {
   record: Record;
+  originalRecord: Record;
+  redToggle = true;
+  yellowToggle = true;
+  greenToggle = true;
 
   constructor(@Inject(MAT_DIALOG_DATA) data: Record) {
+    this.originalRecord = { ...data };
     this.record = data;
   }
 
   getRule(ruleID: string) {
     return this.record.rules.find((x) => x.ruleID === Number(ruleID));
+  }
+
+  toggleChange(event: MatButtonToggleChange) {
+    this.record = { ...this.originalRecord };
+    this.redToggle = this.yellowToggle = this.greenToggle = false;
+    let toggle = event.source;
+
+    if (toggle) {
+      let group = toggle.buttonToggleGroup;
+
+      if (!group.value.some((x: string) => x == 'green'))
+        this.record.validRulesID = [];
+
+      if (!group.value.some((x: string) => x == 'red')) {
+        if (group.value.some((x: string) => x == 'yellow')) {
+          this.record.invalidRulesID = this.record.invalidRulesID.filter(
+            (x) => !this.getRule(x).mandatory
+          );
+        } else {
+          this.record.invalidRulesID = [];
+        }
+      }
+
+      if (!group.value.some((x: string) => x == 'yellow')) {
+        this.record.invalidRulesID = this.record.invalidRulesID.filter(
+          (x) => this.getRule(x).mandatory
+        );
+      }
+
+      group.value.forEach((element: string) => {
+        switch (element) {
+          case 'red':
+            this.redToggle = true;
+            break;
+          case 'yellow':
+            this.yellowToggle = true;
+            break;
+          case 'green':
+            this.greenToggle = true;
+            break;
+        }
+      });
+    } else {
+      this.record.validRulesID = [];
+      this.record.invalidRulesID = [];
+    }
   }
 
   ngOnInit(): void {}
--- angular/src/app/components/rule/rule.module.ts
+++ angular/src/app/components/rule/rule.module.ts
@@ -1,13 +1,19 @@
 import { MatListModule } from '@angular/material/list';
 import { MatDialogModule } from '@angular/material/dialog';
 import { NgModule } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RulesListComponent } from './rules-list/rules-list.component';
 import { EvaluationRulesComponent } from './evaluation-rules/evaluation-rules.component';
 
 @NgModule({
   declarations: [EvaluationRulesComponent, RulesListComponent],
-  imports: [SharedModule, MatDialogModule, MatListModule],
+  imports: [
+    SharedModule,
+    MatDialogModule,
+    MatListModule,
+    MatButtonToggleModule,
+  ],
   exports: [EvaluationRulesComponent, RulesListComponent],
 })
 export class RuleModule {}
