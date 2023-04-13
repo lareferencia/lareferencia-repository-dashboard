3edb97d0a232691faba95946d52977ff28345af4
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-04-13 17:08:09 -0300

    validation functionality

--- angular/src/app/components/template/drawer/drawer.component.ts
+++ angular/src/app/components/template/drawer/drawer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HarvestingService } from 'src/app/core/services/harvesting.service';
 import { MenuService } from 'src/app/core/services/menu.service';
 import { Menu } from 'src/app/shared/models/menu.model';
 
@@ -8,17 +10,30 @@ import { Menu } from 'src/app/shared/models/menu.model';
   styleUrls: ['./drawer.component.css']
 })
 export class DrawerComponent implements OnInit {
-  activeRepository: String;
+  activeRepository: string;
+  harvestingConentId: number;
 
-  constructor( private menuService: MenuService) { }
+  constructor( 
+    private menuService: MenuService,
+    private harvestingService: HarvestingService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
 
     //Subscribe to active repo changes
     this.menuService.activeRepo.subscribe((activeRepo: Menu) => {
-      this.activeRepository = activeRepo?.acronym;
-      console.log(this.activeRepository)
+    this.activeRepository = activeRepo?.acronym
+    this.resultHandler(activeRepo?.acronym);
     });
   }
+  //TODO: Revisat y refactorizar esto porque le pimer peticion falla.
 
+  resultHandler(acronym: string) {
+    this.harvestingService.getHarvestingLastGoodKnowByAcronym(acronym)
+    .subscribe((harvestingContent) => {
+      this.harvestingConentId = harvestingContent.id;
+      console.log(this.harvestingConentId)
+    });
+  }
 }
