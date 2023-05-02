4f3ed43753f97f20fe21d19a1b7aa29c47de47b5
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-05-02 10:48:11 -0300

    pagination harvesting table

--- angular/src/app/harvesting/components/harvesting-table/harvesting-table.component.ts
+++ angular/src/app/harvesting/components/harvesting-table/harvesting-table.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HarvestingTableDataSource } from './harvesting-table-datasource';
 import { HarvestingContent } from 'src/app/shared/models/harvesting-content.model';
+import { LazyLoadEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-harvesting-table',
@@ -12,13 +13,20 @@ import { HarvestingContent } from 'src/app/shared/models/harvesting-content.mode
 })
 export class HarvestingTableComponent implements OnInit {
 
+  acronym: string;
   isLoadingResults = true;
   harvestingContent: HarvestingContent[];
   dataSource: HarvestingTableDataSource;
-  pageSize = 30;
+  pageSize: number = 10;
+  pageNumber: number;
+  totalRecords: number;
+  isLoading: boolean = true;
+
+
   csvData: any[];
   headerData: any[];
   admUser = false;
+
 
 
   constructor(
@@ -29,24 +37,33 @@ export class HarvestingTableComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.acronym = this.route.snapshot.paramMap.get('acronym');
+  }
 
-    const acronym = this.route.snapshot.paramMap.get('acronym');
+  loadContent(event: LazyLoadEvent){
+    this.isLoading = true;
 
-    this.harvestingService.getHarvestingHistoryByAcronym(acronym, 0, this.pageSize)
-      .subscribe(({content}) => {
-        this.harvestingContent = content.map((harvestingContent) => {
+    this.pageNumber = event.first / event.rows;
+    this.pageSize = event.rows;
+
+    
+    this.harvestingService.getHarvestingHistoryByAcronym(this.acronym, this.pageNumber, this.pageSize)
+      .subscribe((result) => {
+
+        this.isLoading = false;
+        this.totalRecords = result.totalElements;
+        this.harvestingContent = result.content.map((harvestingContent) => {
           return {
             ...harvestingContent,
-            invalidRecords: harvestingContent.harvestedSize - harvestingContent.validSize,
-            
+            invalidRecords: harvestingContent.harvestedSize - harvestingContent.validSize,    
           };
         }).sort(
           (a, b) =>
             new Date(b.startTime).getTime() -
             new Date(a.startTime).getTime()
         );
-        console.log(this.harvestingContent);        
       });
+
   }
 
   detailClick(harvestingContent: HarvestingContent){
