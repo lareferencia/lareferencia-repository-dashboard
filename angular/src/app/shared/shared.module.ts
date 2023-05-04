79f2369db068d1faf4ae211309bbefbf5df9ce3d
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-05-04 08:56:21 -0300

    Update shared.module.ts

--- angular/src/app/shared/shared.module.ts
+++ angular/src/app/shared/shared.module.ts
@@ -7,19 +7,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatButtonModule } from '@angular/material/button';
 import { ValidationCardComponent } from './components/validation-card/validation-card.component';
 import { InfoCardComponent } from './components/info-card/info-card.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [...fromComponents.components, ...fromDirectives.directives, InfoCardComponent],
+  declarations: [...fromComponents.components, ...fromDirectives.directives, InfoCardComponent, ValidationCardComponent],
   imports: [
     CommonModule,
     FormsModule,
     MatCardModule,
     MatProgressBarModule,
     MatIconModule,
-    MatButtonModule,
+    MaterialModule
   ],
   exports: [
     FormsModule,
