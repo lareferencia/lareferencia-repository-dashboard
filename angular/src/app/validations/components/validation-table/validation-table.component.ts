af9270f77915625c61e00e0c1cadcbab639a4979
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-05-04 08:55:21 -0300

    filter styles

--- angular/src/app/validations/components/validation-table/validation-table.component.ts
+++ angular/src/app/validations/components/validation-table/validation-table.component.ts
@@ -1,15 +1,15 @@
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
-import { ValidationDetailComponent } from './../validation-detail/validation-detail.component';
 import { Validation } from '../../../shared/models/validation.model';
 import { Rule } from '../../../shared/models/rule.model';
-import { Component, OnInit, ViewChild, Input, ElementRef, AfterViewInit } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { ValidationTableDataSource } from './validation-table-datasource';
-import { MatPaginator } from '@angular/material/paginator';
-import { startWith, tap, delay } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
+
+
+interface MandatoryOption {
+  name: string;
+  value: boolean;
+}
 
 @Component({
   selector: 'app-validation-table',
@@ -20,22 +20,22 @@ export class ValidationTableComponent implements AfterViewInit, OnInit {
   
   @Input() validation: Validation;
 
-
   dataSource: Rule[];
   harvestingID: number;
   acronym: string;
   csvData: any[];
   headerData: any[];
-  requiredRule? = true;
   admUser = false;
+  mandatoryOptions: MandatoryOption[];
+  isMandatory: MandatoryOption;
 
-  displayedColumns: string[] = [
-    'ruleID',
-    'name',
-    'description',
-    'mandatory',
-    'conformity',
-    'validCount',
+  displayedColumns: any[] = [
+    {value: 'ruleID', name: 'ID'},
+    {value: 'name', name: 'Name'},
+    {value: 'description', name: 'Description'},
+    {value: 'mandatory', name: 'Mandatory'},
+    {value: 'conformity', name: 'Conformity'},
+    {value: 'validCount', name: 'Valid records'},
   ];
 
   constructor(
@@ -50,21 +50,25 @@ export class ValidationTableComponent implements AfterViewInit, OnInit {
       this.route.snapshot.paramMap.get('harvestingID')
     );
     this.acronym = this.route.snapshot.paramMap.get('acronym');
+    this.mandatoryOptions = [
+      {name: 'Mandatory', value: true},
+      {name: 'Not mandatory', value: false},
+      {name: 'All', value: null}
+    ];
   }
-
 
   ngAfterViewInit() {
     this.loadRecords()
   }
 
   loadRecords() {
-    let rules = this.validation.rulesByID;
-    this.dataSource = rules;
+    this.dataSource = this.validation.rulesByID;
 
-    // if (this.requiredRule != null)
-    //   rules = this.validation.rulesByID.filter((x) => x.mandatory == this.requiredRule);
+    if (this.isMandatory && this.isMandatory.value !== null)
 
-    // this.dataSource = new ValidationTableDataSource(rules);
+      this.dataSource = this.validation.rulesByID
+        .filter((x) => x.mandatory == this.isMandatory.value);
+
 
     // this.csvData = this.validation.rulesByID.map((x) => {
     //   return {
