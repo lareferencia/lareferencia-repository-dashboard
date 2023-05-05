871f8447fc0d94d9dae8336359ca5a40e2d9b311
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-05-05 13:19:32 -0300

    sorting harvesting table

--- angular/src/app/harvesting/components/harvesting-table/harvesting-table.component.ts
+++ angular/src/app/harvesting/components/harvesting-table/harvesting-table.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HarvestingTableDataSource } from './harvesting-table-datasource';
 import { HarvestingContent } from 'src/app/shared/models/harvesting-content.model';
-import { LazyLoadEvent } from 'primeng/api';
+import { LazyLoadEvent, SortEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-harvesting-table',
@@ -17,17 +17,46 @@ export class HarvestingTableComponent implements OnInit {
   isLoadingResults = true;
   harvestingContent: HarvestingContent[];
   dataSource: HarvestingTableDataSource;
-  pageSize: number = 10;
+  pageSize: number = 80;
   pageNumber: number;
   totalRecords: number;
   isLoading: boolean = true;
 
+  filter = {
+    sortField: '',
+    sortOrder: 1,
+  };
+
+  displayColumns = [
+    {
+      title: 'ID',
+      field: 'id',
+    },
+    {
+      title: 'Harvested Record',
+      field: 'harvestedSize',
+    },
+    {
+      title: 'Valid Records',
+      field: 'validSize',
+    },
+    {
+      title: 'Invalid Records',
+      field: 'invalidRecords',
+    },
+    {
+      title: 'Start',
+      field: 'startTime',
+    },
+    {
+      title: 'End',
+      field: 'endTime',
+    },
+  ]
 
   csvData: any[];
   headerData: any[];
   admUser = false;
-
-
 
   constructor(
     private harvestingService: HarvestingService,
@@ -40,14 +69,32 @@ export class HarvestingTableComponent implements OnInit {
     this.acronym = this.route.snapshot.paramMap.get('acronym');
   }
 
+  onSort(event: SortEvent) {
+    this.filter.sortField = event.field;
+    this.filter.sortOrder = event.order === 1 ? 1 : -1;
+    this.loadContent({
+      first: 0,
+      rows: this.pageSize,
+      sortField: this.filter.sortField,
+      sortOrder: this.filter.sortOrder
+    });
+  }
+
   loadContent(event: LazyLoadEvent){
+    console.log(event);
+    
     this.isLoading = true;
 
     this.pageNumber = event.first / event.rows;
     this.pageSize = event.rows;
 
     
-    this.harvestingService.getHarvestingHistoryByAcronym(this.acronym, this.pageNumber, this.pageSize)
+    this.harvestingService.getHarvestingHistoryByAcronym(
+      this.acronym, 
+      this.pageNumber, 
+      this.pageSize,
+      this.filter.sortField,
+      this.filter.sortOrder)
       .subscribe((result) => {
 
         this.isLoading = false;
