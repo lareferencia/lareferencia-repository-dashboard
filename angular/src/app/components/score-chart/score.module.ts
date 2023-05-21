dc79cef240fbf414fd995b2621410c671d6ce8f3
Juan Sebastian Manitta <manittajuan@gmail.com>
2023-05-21 08:07:40 -0300

    deleting score-chart

--- angular/src/app/components/score-chart/score.module.ts
+++
@@ -1,9 +0,0 @@
-import { ScoreChartComponent } from './score-chart.component';
-import { NgModule } from '@angular/core';
-import { SharedModule } from 'src/app/shared/shared.module';
-
-@NgModule({
-  declarations: [ScoreChartComponent],
-  imports: [SharedModule],
-})
-export class ScoreModule {}
