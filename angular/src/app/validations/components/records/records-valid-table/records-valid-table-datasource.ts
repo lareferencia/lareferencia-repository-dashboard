daad30afbe565edff1e76d595260d3c9e9bdc28c
Juan Sebastian Manitta <manittajuan@gmail.com>
2023-06-06 08:32:52 -0300

    deleting angular material unused modules

--- angular/src/app/validations/components/records/records-valid-table/records-valid-table-datasource.ts
+++ angular/src/app/validations/components/records/records-valid-table/records-valid-table-datasource.ts
@@ -1,79 +1,79 @@
-import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { map } from 'rxjs/operators';
-import { Observable, of as observableOf, merge } from 'rxjs';
-import { Record } from 'src/app/shared/models/record.model';
+// import { DataSource } from '@angular/cdk/collections';
+// import { MatPaginator } from '@angular/material/paginator';
+// import { MatSort } from '@angular/material/sort';
+// import { map } from 'rxjs/operators';
+// import { Observable, of as observableOf, merge } from 'rxjs';
+// import { Record } from 'src/app/shared/models/record.model';
 
-/**
- * Data source for the RecordsTable view. This class should
- * encapsulate all logic for fetching and manipulating the displayed data
- * (including sorting, pagination, and filtering).
- */
-export class RecordsTableDataSource extends DataSource<Record> {
-  data: Record[];
-  paginator: MatPaginator;
-  sort: MatSort;
+// /**
+//  * Data source for the RecordsTable view. This class should
+//  * encapsulate all logic for fetching and manipulating the displayed data
+//  * (including sorting, pagination, and filtering).
+//  */
+// export class RecordsTableDataSource extends DataSource<Record> {
+//   data: Record[];
+//   paginator: MatPaginator;
+//   sort: MatSort;
 
-  constructor(value: Record[]) {
-    super();
-    this.data = value;
-  }
+//   constructor(value: Record[]) {
+//     super();
+//     this.data = value;
+//   }
 
-  /**
-   * Connect this data source to the table. The table will only update when
-   * the returned stream emits new items.
-   * @returns A stream of the items to be rendered.
-   */
-  connect(): Observable<Record[]> {
-    // Combine everything that affects the rendered data into one update
-    // stream for the data-table to consume.
-    const dataMutations = [
-      observableOf(this.data),
-      this.paginator.page,
-      this.sort.sortChange
-    ];
+//   /**
+//    * Connect this data source to the table. The table will only update when
+//    * the returned stream emits new items.
+//    * @returns A stream of the items to be rendered.
+//    */
+//   connect(): Observable<Record[]> {
+//     // Combine everything that affects the rendered data into one update
+//     // stream for the data-table to consume.
+//     const dataMutations = [
+//       observableOf(this.data),
+//       this.paginator.page,
+//       this.sort.sortChange
+//     ];
 
-    return merge(...dataMutations).pipe(map(() => {
-      return this.getPagedData(this.getSortedData([...this.data]));
-    }));
-  }
+//     return merge(...dataMutations).pipe(map(() => {
+//       return this.getPagedData(this.getSortedData([...this.data]));
+//     }));
+//   }
 
-  /**
-   *  Called when the table is being destroyed. Use this function, to clean up
-   * any open connections or free any held resources that were set up during connect.
-   */
-  disconnect() {}
+//   /**
+//    *  Called when the table is being destroyed. Use this function, to clean up
+//    * any open connections or free any held resources that were set up during connect.
+//    */
+//   disconnect() {}
 
-  /**
-   * Paginate the data (client-side). If you're using server-side pagination,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getPagedData(data: Record[]) {
-    return data;
-  }
+//   /**
+//    * Paginate the data (client-side). If you're using server-side pagination,
+//    * this would be replaced by requesting the appropriate data from the server.
+//    */
+//   private getPagedData(data: Record[]) {
+//     return data;
+//   }
 
-  /**
-   * Sort the data (client-side). If you're using server-side sorting,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getSortedData(data: Record[]) {
-    if (!this.sort.active || this.sort.direction === '') {
-      return data;
-    }
+//   /**
+//    * Sort the data (client-side). If you're using server-side sorting,
+//    * this would be replaced by requesting the appropriate data from the server.
+//    */
+//   private getSortedData(data: Record[]) {
+//     if (!this.sort.active || this.sort.direction === '') {
+//       return data;
+//     }
 
-    return data.sort((a, b) => {
-      const isAsc = this.sort.direction === 'asc';
-      switch (this.sort.active) {
-        case 'identifier': return compare(a.identifier, b.identifier, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
-        default: return 0;
-      }
-    });
-  }
-}
+//     return data.sort((a, b) => {
+//       const isAsc = this.sort.direction === 'asc';
+//       switch (this.sort.active) {
+//         case 'identifier': return compare(a.identifier, b.identifier, isAsc);
+//         case 'id': return compare(+a.id, +b.id, isAsc);
+//         default: return 0;
+//       }
+//     });
+//   }
+// }
 
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-function compare(a: string | number, b: string | number, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-}
+// /** Simple sort comparator for example ID/Name columns (for client-side sorting). */
+// function compare(a: string | number, b: string | number, isAsc: boolean) {
+//   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+// }
--- angular/src/app/validations/components/records/records-valid-table/records-valid-table.component.ts
+++ angular/src/app/validations/components/records/records-valid-table/records-valid-table.component.ts
@@ -1,100 +1,100 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { startWith, tap } from 'rxjs/operators';
+// import { Component, OnInit, ViewChild, Input } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { startWith, tap } from 'rxjs/operators';
 
-import { ValidationService } from 'src/app/core/services/validation.service';
-import { RecordsTableDataSource } from './records-valid-table-datasource';
+// import { ValidationService } from 'src/app/core/services/validation.service';
+// import { RecordsTableDataSource } from './records-valid-table-datasource';
 
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { MatDialogConfig, MatDialog } from '@angular/material/dialog';
+// import { MatPaginator } from '@angular/material/paginator';
+// import { MatSort } from '@angular/material/sort';
+// import { MatTable } from '@angular/material/table';
+// import { MatDialogConfig, MatDialog } from '@angular/material/dialog';
 
-import { Record } from 'src/app/shared/models/record.model';
-import { Validation } from 'src/app/shared/models/validation.model';
+// import { Record } from 'src/app/shared/models/record.model';
+// import { Validation } from 'src/app/shared/models/validation.model';
 
-import { EvaluationRulesComponent } from '../../rule/evaluation-rules/evaluation-rules.component';
+// import { EvaluationRulesComponent } from '../../rule/evaluation-rules/evaluation-rules.component';
 
-@Component({
-  selector: 'app-records-valid-table',
-  templateUrl: './records-valid-table.component.html',
-  styleUrls: ['./records-valid-table.component.css'],
-})
-export class RecordsValidTableComponent implements OnInit {
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatTable) table: MatTable<Record>;
-  @Input() validation: Validation;
-  dataSource: RecordsTableDataSource;
-  pageSize = 25;
-  harvestingID: number;
-  acronym: string;
-  ruleID: number;
-  isLoadingResults = true;
-  csvData: any[];
+// @Component({
+//   selector: 'app-records-valid-table',
+//   templateUrl: './records-valid-table.component.html',
+//   styleUrls: ['./records-valid-table.component.css'],
+// })
+// export class RecordsValidTableComponent implements OnInit {
+//   @ViewChild(MatPaginator) paginator: MatPaginator;
+//   @ViewChild(MatSort) sort: MatSort;
+//   @ViewChild(MatTable) table: MatTable<Record>;
+//   @Input() validation: Validation;
+//   dataSource: RecordsTableDataSource;
+//   pageSize = 25;
+//   harvestingID: number;
+//   acronym: string;
+//   ruleID: number;
+//   isLoadingResults = true;
+//   csvData: any[];
   
-  displayedColumns = [
-    'id',
-    'identifier',
-    'isValid',
-    'isTransformed',
-    'button-detail',
-  ];
+//   displayedColumns = [
+//     'id',
+//     'identifier',
+//     'isValid',
+//     'isTransformed',
+//     'button-detail',
+//   ];
 
-  constructor(
-    private validationService: ValidationService,
-    private route: ActivatedRoute,
-    private dialog: MatDialog
-  ) {}
+//   constructor(
+//     private validationService: ValidationService,
+//     private route: ActivatedRoute,
+//     private dialog: MatDialog
+//   ) {}
 
-  ngOnInit() {
-    this.harvestingID = Number(this.route.snapshot.paramMap.get('harvestingID'));
-    this.acronym = this.route.snapshot.paramMap.get('acronym');
-    this.ruleID = Number(this.route.snapshot.paramMap.get('ruleID'));
-  }
+//   ngOnInit() {
+//     this.harvestingID = Number(this.route.snapshot.paramMap.get('harvestingID'));
+//     this.acronym = this.route.snapshot.paramMap.get('acronym');
+//     this.ruleID = Number(this.route.snapshot.paramMap.get('ruleID'));
+//   }
 
-  detailClick(record: Record): void {
-    record.rules = this.validation.rulesByID;
+//   detailClick(record: Record): void {
+//     record.rules = this.validation.rulesByID;
 
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.data = record;
-    dialogConfig.autoFocus = false;
+//     const dialogConfig = new MatDialogConfig();
+//     dialogConfig.data = record;
+//     dialogConfig.autoFocus = false;
 
-    this.dialog.open(EvaluationRulesComponent, dialogConfig);
-  }
+//     this.dialog.open(EvaluationRulesComponent, dialogConfig);
+//   }
   
-  ngAfterViewInit() {
-    this.paginator.page
-      .pipe(
-        startWith(null),
-        tap(() => {
-          this.isLoadingResults = true;
-          this.validationService
-            .getRecordsByHarvestingIDValidRuleID(
-              this.acronym,
-              this.harvestingID,
-              this.ruleID,
-              this.paginator.pageIndex,
-              this.paginator.pageSize
-            )
-            .subscribe((result) => {
-              this.dataSource = new RecordsTableDataSource(result.content);
-              this.paginator.length = result.totalElements;
-              this.dataSource.paginator = this.paginator;
-              this.dataSource.sort = this.sort;
-              this.isLoadingResults = false;
+//   ngAfterViewInit() {
+//     this.paginator.page
+//       .pipe(
+//         startWith(null),
+//         tap(() => {
+//           this.isLoadingResults = true;
+//           this.validationService
+//             .getRecordsByHarvestingIDValidRuleID(
+//               this.acronym,
+//               this.harvestingID,
+//               this.ruleID,
+//               this.paginator.pageIndex,
+//               this.paginator.pageSize
+//             )
+//             .subscribe((result) => {
+//               this.dataSource = new RecordsTableDataSource(result.content);
+//               this.paginator.length = result.totalElements;
+//               this.dataSource.paginator = this.paginator;
+//               this.dataSource.sort = this.sort;
+//               this.isLoadingResults = false;
               
-              this.csvData = result.content.map((x) => {
-                return {
-                  id: x.id,
-                  identifier: x.identifier,
-                  validation: x.isValid,
-                  tranformation: x.isTransformed,
-                };
-              });
-            });
-        })
-      )
-      .subscribe(() => {});
-  }
-}
+//               this.csvData = result.content.map((x) => {
+//                 return {
+//                   id: x.id,
+//                   identifier: x.identifier,
+//                   validation: x.isValid,
+//                   tranformation: x.isTransformed,
+//                 };
+//               });
+//             });
+//         })
+//       )
+//       .subscribe(() => {});
+//   }
+// }
