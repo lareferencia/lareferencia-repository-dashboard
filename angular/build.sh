70c2ce474d95905683627570914024543f348db3
lmatas <lmatas@gmail.com>
2023-06-07 15:43:00 +0000

    fix build script

--- angular/build.sh
+++ angular/build.sh
@@ -1,8 +1,8 @@
-ng build --output-path=dist/es --base-href /es/ --localize=es
-ng build --output-path=dist/pt --base-href /pt/ --localize=pt
-ng build --output-path=dist/en --base-href /en/ --localize=en
+ng build --localize 
+#ng build --output-path=dist/es --base-href /es/ --localize=es
+#ng build --output-path=dist/pt --base-href /pt/ --localize=pt
+#ng build --output-path=dist/en --base-href /en/ --localize=en
 
-sudo cp -r dist/es /var/www/dashboard
-sudo cp -r dist/pt /var/www/dashboard
-sudo cp -r dist/en /var/www/dashboard
+sudo cp -r dist/frontend/* /var/www/dashboard
+
 
