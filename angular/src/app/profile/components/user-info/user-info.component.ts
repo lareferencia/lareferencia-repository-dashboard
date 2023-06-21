69789d595c7de8fc0dccbb38b12f7f33b1bb49e2
Juan Sebastian Manitta <manittajuan@gmail.com>
2023-06-21 13:13:12 -0300

    reactive form in user info

--- angular/src/app/profile/components/user-info/user-info.component.ts
+++ angular/src/app/profile/components/user-info/user-info.component.ts
@@ -5,6 +5,7 @@ import { AuthenticationService } from 'src/app/core/services/authentication.serv
 
 import { MessageService } from 'primeng/api';
 import { PositionType } from 'src/app/shared/enums/user-position';
+import { FormBuilder, FormGroup , Validators } from '@angular/forms';
 
 interface PositionOptions{
   label:string;
@@ -17,27 +18,45 @@ interface PositionOptions{
   styleUrls: ['./user-info.component.css'],
   providers: [MessageService]
 })
-
-
 export class UserInfoComponent implements OnInit {
+
   public updatingSuccess = true;
   public userName: string;
-  public user: any;
   public positions: PositionOptions[] = [];
+  public userForm: FormGroup;
+  public isLoading = true;
  
   constructor(
     private manageUsersService: ManageUsersService,
     private authenticationService: AuthenticationService,
-    private messageService: MessageService
-  ) {}
+    private messageService: MessageService,
+    private fb: FormBuilder,
+  ) {
+    this.userForm = this.fb.group({
+      first_name: [''],
+      email: ['', [ Validators.required, Validators.email ]],
+      telephone: [''],
+      affiliation: [''],
+      position: [''],
+    });
+  }
 
   async ngOnInit(): Promise<void> {
+    this.isLoading = true;
     this.userName = await this.authenticationService.getUserName();
     this.manageUsersService
       .getUser(this.userName)
-      .subscribe((result) => {
-       this.user = result
-       this.user.position = parseInt(this.user.position);
+      .subscribe((result: any) => {
+
+        this.isLoading = false;
+
+        this.userForm.patchValue({
+          first_name: result.first_name,
+          email: result.email,
+          telephone: result.telephone,
+          affiliation: result.affiliation,
+          position: parseInt(result.position, 10),
+        })
       });
       
       this.positions = [
@@ -53,8 +72,12 @@ export class UserInfoComponent implements OnInit {
   onClickSave() {
     this.updatingSuccess = false;
 
-    if( this.userName.length < 1) return;
-    this.manageUsersService.updateUser(this.userName, this.user).subscribe(
+    if( this.userForm.invalid ) {
+      this.updatingSuccess = true;
+      return;
+    };
+    
+    this.manageUsersService.updateUser(this.userName, this.userForm.value).subscribe(
       (result) => this.resultHandler(result),
       () => this.resultHandler(false)
     );
