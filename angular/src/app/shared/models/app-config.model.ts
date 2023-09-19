9cab871ea911e30f7324d3d4ebb189b6f212d1a0
Juan Sebastian Manitta <manittajuan@gmail.com>
2023-09-19 17:01:08 +0200

    updated app config interface

--- angular/src/app/shared/models/app-config.model.ts
+++ angular/src/app/shared/models/app-config.model.ts
@@ -1,65 +1,81 @@
-
 export interface AppConfig {
-    authentication_module: AuthenticationModule;
-    main_module:           MainModule;
-    historic_module:       Module;
-    validation_module:     Module;
-    statistics_module:     StatisticsModule;
-    broker_module:         BrokerModule;
-  }
-  
-  export interface AuthenticationModule {
-    key_cloack_config: KeyCloackConfig;
-  }
-  
-  export interface KeyCloackConfig {
-    url:      string;
-    realm:    string;
-    clientId: string;
-  }
-  
-  export interface BrokerModule {
-    endpoints: BrokerModuleEndpoints;
-  }
-  
-  export interface BrokerModuleEndpoints {
-    brokerService: string;
-  }
-  
-  export interface Module {
-    endpoints: HistoricModuleEndpoints;
-  }
-  
-  export interface HistoricModuleEndpoints {
-    harvestingService: string;
-    validationService: string;
-  }
-  
-  export interface MainModule {
-    endpoints: MainModuleEndpoints;
-  }
-  
-  export interface MainModuleEndpoints {
-    securityService: string;
-  }
-  
-  export interface StatisticsModule {
-    active:          boolean;
-    event_labels:    EventLabels;
-    scope_labels:    ScopeLabels;
-    country:         string;
-    national_source: string;
-    preview:         boolean;
-  }
-  
-  export interface EventLabels {
-    view:     string;
-    download: string;
-    outlink:  string;
-  }
-  
-  export interface ScopeLabels {
-    L: string;
-    N: string;
-    R: string;
-  }
+  authentication_module: AuthenticationModule;
+  main_module:           MainModule;
+  admin_module:          AdminModule;
+  historic_module:       Module;
+  validation_module:     Module;
+  statistics_module:     StatisticsModule;
+  broker_module:         BrokerModule;
+}
+
+export interface AdminModule {
+  active:    boolean;
+  endpoints: AdminModuleEndpoints;
+}
+
+export interface AdminModuleEndpoints {
+  securityService: string;
+}
+
+export interface AuthenticationModule {
+  active:            boolean;
+  endpoints:         AuthenticationModuleEndpoints;
+  key_cloack_config: KeyCloackConfig;
+}
+
+export interface AuthenticationModuleEndpoints {
+  authService: string;
+}
+
+export interface KeyCloackConfig {
+  realm:    string;
+  clientId: string;
+}
+
+export interface BrokerModule {
+  active:    boolean;
+  endpoints: BrokerModuleEndpoints;
+}
+
+export interface BrokerModuleEndpoints {
+  brokerService: string;
+}
+
+export interface Module {
+  active:    boolean;
+  endpoints: HistoricModuleEndpoints;
+}
+
+export interface HistoricModuleEndpoints {
+  harvestingService: string;
+  validationService: string;
+}
+
+export interface MainModule {
+}
+
+export interface StatisticsModule {
+  active:         boolean;
+  historic_stats: HistoricStats;
+}
+
+export interface HistoricStats {
+  widget_url:      string;
+  event_labels:    EventLabels;
+  scope_labels:    ScopeLabels;
+  country:         string;
+  national_source: string;
+  preview:         boolean;
+}
+
+export interface EventLabels {
+  view:     string;
+  download: string;
+  outlink:  string;
+}
+
+export interface ScopeLabels {
+  L: string;
+  N: string;
+  R: string;
+}
