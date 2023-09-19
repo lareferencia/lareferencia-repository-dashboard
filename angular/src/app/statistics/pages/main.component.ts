2074cddc563c4779b91b21881a30d63143a7f12a
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-09-19 12:43:43 +0200

    using json config in statistics module

--- angular/src/app/statistics/pages/main.component.ts
+++ angular/src/app/statistics/pages/main.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HarvestingService } from 'src/app/core/services/harvesting.service';
 import { MenuService } from 'src/app/core/services/menu.service';
 import { switchMap } from 'rxjs/operators';
-import { AppConfigService } from '../../core/services/loadAppConfig.service';
+import { AppConfigService } from '../../core/services/app-config.service';
+import { StatisticsModule } from 'src/app/shared/models/app-config.model';
 
 @Component({
   selector: 'app-main',
@@ -22,20 +23,19 @@ export class MainComponent implements OnInit {
 
   ngOnInit(): void {
 
-    const widgetConfig = this.appConfigService.getConfig()
+    const widgetConfig = this.appConfigService.getStatisticsModuleConfig()
     console.log(widgetConfig);
-    
     
     this.menuSrvice.activeRepo.pipe(
       switchMap(repo => {
         return this.harvestingSrvice.getHarvestingByAcronym(repo.acronym)
       })
     ).subscribe(data => {
-      this.resultHandler(data.attributes.stats_source_id)
+      this.resultHandler(data.attributes.stats_source_id, widgetConfig)
     });    
   }
 
-  resultHandler( stats_source_id:string ){
+  resultHandler( stats_source_id:string, widgetConfig: StatisticsModule ){
 
     window['lrhw'] = {
       //identifier: ´´,
@@ -43,19 +43,19 @@ export class MainComponent implements OnInit {
       //identifier_prefix: '',
       //identifier_regex: '',
       event_labels: {
-        view: 'Vistas',
-        download: 'Descargas',
-        outlink: 'Enlaces'
+        view: widgetConfig.event_labels.view,
+        download: widgetConfig.event_labels.download,
+        outlink: widgetConfig.event_labels.outlink
       },
       scope_labels: {
-        L: 'LA Referencia',
-        N: '[[Nombre nodo nacional]]',
-        R: '[[Nombre repositorio]]'
+        L:  widgetConfig.scope_labels.L,
+        N: widgetConfig.scope_labels.N,
+        R:  widgetConfig.scope_labels.R
       },
-      country: '[[ISO-país]]',
-      national_source: '[[SITEID::XXX]]',
+      country: widgetConfig.country,
+      national_source:  widgetConfig.national_source,
       repository_source: stats_source_id,
-      preview: false,
+      preview: widgetConfig.preview,
     };
 
     const widget = document.createElement('script');
