4f4e243d2134a1e2b22bb114969caccee9288eb7
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-10-10 17:20:13 +0200

    network editor

---
+++ angular/src/app/harvesting-admin/pages/components/network-editor/network-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-network-editor',
+  templateUrl: './network-editor.component.html',
+  styleUrls: ['./network-editor.component.css']
+})
+export class NetworkEditorComponent {
+
+}
