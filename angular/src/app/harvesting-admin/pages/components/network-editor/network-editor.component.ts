128f723d18d63c9fb444ffa5de9f17539beb9b0c
Juan Sebastian Manitta <106455530+JuanManitta@users.noreply.github.com>
2023-10-11 11:43:51 +0200

    first commit

--- angular/src/app/harvesting-admin/pages/components/network-editor/network-editor.component.ts
+++ angular/src/app/harvesting-admin/pages/components/network-editor/network-editor.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-network-editor',
   templateUrl: './network-editor.component.html',
   styleUrls: ['./network-editor.component.css']
 })
-export class NetworkEditorComponent {
+export class NetworkEditorComponent implements OnInit {
 
+  constructor( private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getFormFields();
+  }
+  getFormFields(){
+    return this.http.get('./assets/data/prueba.json').subscribe(data => {
+      console.log(data)
+    });
+  }
 }
